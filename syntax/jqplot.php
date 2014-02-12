aa98e4274f9487c87d434536f39d9e3c652a5812
Satoshi Sahara <sahara.satoshi@gmail.com>
2014-02-13 01:21:41 +0900

    test3

---
+++ syntax/jqplot.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * DokuWiki Syntax Plugin Canvas jqplot
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author  Sahara Satoshi <sahara.satoshi@gmail.com>
+ *
+ * SYNTAX:
+ *        <jqplot chartid width,height>
+ *         ... javascript ...
+ *        </jqplot>
+ */
+require_once DOKU_INC.'lib/plugins/canvas/canvas.php';
+
+class syntax_plugin_canvas_jqplot extends syntax_plugin_canvas_canvas {
+
+    protected $entry_pattern = '<jqplot.*?>(?=.*?</jqplot>)';
+    protected $exit_pattern  = '</jqplot>';
+
+}
